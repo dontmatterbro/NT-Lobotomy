26a3640f02fdeac3dd4a2f8d44681a0550cd2979
dontmatterbro <yh846175@gmail.com>
2024-09-03 11:14:51 +0300

    item, surgery code
    added orbitoclast
    added surgical hammer
    
    added lobotomy surgery code base

--- Lua/Scripts/Server/surgery.lua
+++ Lua/Scripts/Server/surgery.lua
@@ -1,5 +1,6 @@
 --Orbitoclast
 --Use hammer to insert orbitoclast to skull
+--doesnt require anesthesia only local numbness
 
 Hook.Add("item.applyTreatment", "NT.itemused", function(item, usingCharacter, targetCharacter, limb)
     
@@ -15,14 +16,40 @@ Hook.Add("item.applyTreatment", "NT.itemused", function(item, usingCharacter, ta
 	
 	---------------------------------------SURGERY CODE--------------------------
 	
+	if not limbtype==11 then return end
+	if HF.HasAffliction(targetCharacter, "stasis") then return end 
+	
+	--frontal lobotomy
 	if --Orbitoclast
 		identifier == "orbitoclast"
 	then
-	
+		HF.AddAfflictionLimb(targetCharacter, "orbitoclastready", 11, 3)
 	end
 	
-
-
-
+	
+	if --Hammer
+			identifier == "surgicalhammer"
+		and HF.HasAffliction(targetCharacter, "orbitoclastready")
+	
+	then
+		if --Cause pain if not anesthesia
+			not HF.CanPerformSurgeryOn(targetCharacter)
+		then
+			HF.AddAfflictionLimb(targetCharacter, "severepainlite", 11, 5)
+			HF.AddAfflictionLimb(targetCharacter, "pain_extremity", 11, 100)
+		end
+		
+		HF.AddAfflictionLimb(targetCharacter, "orbitoclastinsertion", 11, 3)
+	end
+	--need a way to shake orbitoclast after insertion. maybe player controls somehow??
+	
+	--ethanol lobotomy
+	if --ethanol insertion
+			identifier == "ethanol"
+		and HF.HasAffliction(targetCharacter, "drilledbones") --bugtest this
+	then
+		--lobotomy code here
+	end
+	
 
 end)
