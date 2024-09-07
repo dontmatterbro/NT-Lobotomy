78aa9e0b79a350285aa242256c08ebbbb725194b
dontmatterbro <yh846175@gmail.com>
2024-09-07 18:23:59 +0300

    v 1.0.0h1 release
    fixed CRC crafting

--- Lua/Autorun/init.lua
+++ Lua/Autorun/init.lua
@@ -1,7 +1,7 @@
 NTLOBO = {} -- Neurotrauma Lobotomy
 NTLOBO.Name="Lobotomy"
-NTLOBO.Version = "A1.0.0"
-NTLOBO.VersionNum = 010000000
+NTLOBO.Version = "A1.0.0h1"
+NTLOBO.VersionNum = 010000001
 NTLOBO.MinNTVersion = "A1.9.5h4"
 NTLOBO.MinNTVersionNum = 01090504
 NTLOBO.Path = table.pack(...)[1]
