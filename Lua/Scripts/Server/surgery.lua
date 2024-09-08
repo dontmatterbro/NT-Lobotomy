4586260ebec63140e6cd53d314ee02acff9f0e76
dontmatterbro <yh846175@gmail.com>
2024-09-08 19:22:04 +0300

    fuckup reversed

--- Lua/Scripts/Server/surgery.lua
+++ Lua/Scripts/Server/surgery.lua
@@ -122,7 +122,7 @@ Hook.Add("item.applyTreatment", "NTLOBO.itemused", function(item, usingCharacter
 			identifier == "nervegenerators"
 		and limbtype==11
 		and HF.HasAffliction(targetCharacter, "drilledbones", 90) 
-		-- and HF.HasAffliction(targetCharacter, "lobotomy") adverse effects present now
+		and HF.HasAffliction(targetCharacter, "lobotomy")
 	then
 		HF.AddAfflictionLimb(targetCharacter, "nervegeneration", 11, 1) --this will remove lobotomies upon reaching 100%
 		Entity.Spawner.AddItemToRemoveQueue(item)
@@ -160,8 +160,7 @@ Hook.Add("nerveregen", function(effect, deltaTime, item, targets, worldPosition,
 
 	for k, targetCharacter in pairs(targets) do
 		if --remove lobotomy
-				HF.HasAffliction(targetCharacter, "nervegeneration", 98)
-			and HF.HasAffliction(targetCharacter, "lobotomy")
+			HF.HasAffliction(targetCharacter, "nervegeneration", 98)
 		then
 			targetCharacter.CharacterHealth.ReduceAfflictionOnAllLimbs("nervegeneration", 1000)
 			
@@ -178,13 +177,6 @@ Hook.Add("nerveregen", function(effect, deltaTime, item, targets, worldPosition,
 			for RemoveBadLobotomy in BadLobotomyAfflictions do
 				targetCharacter.CharacterHealth.ReduceAfflictionOnAllLimbs(RemoveBadLobotomy, 1000)
 			end
-		
-		elseif 
-					HF.HasAffliction(targetCharacter, "nervegeneration", 98)
-			and not HF.HasAffliction(targetCharacter, "lobotomy")
-		then
-			targetCharacter.CharacterHealth.ReduceAfflictionOnAllLimbs("nervegeneration", 1000)
-			HF.AddAfflictionLimb(targetCharacter, "nervedeath", 11, 1)
 		end
 	end
 
