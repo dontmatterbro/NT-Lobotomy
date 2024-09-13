f7f413d88caee6e625f90a5df0ee3e3483051b6b
dontmatterbro <yh846175@gmail.com>
2024-09-13 14:55:34 +0300

    v 1.0.1h1 release

--- Lua/Autorun/init.lua
+++ Lua/Autorun/init.lua
@@ -1,6 +1,6 @@
 NTLOBO = {} -- Neurotrauma Lobotomy
 NTLOBO.Name="Lobotomy"
-NTLOBO.Version = "A1.0.1"
+NTLOBO.Version = "A1.0.1h1"
 NTLOBO.VersionNum = 010000101
 NTLOBO.MinNTVersion = "A1.9.5h4"
 NTLOBO.MinNTVersionNum = 01090504
