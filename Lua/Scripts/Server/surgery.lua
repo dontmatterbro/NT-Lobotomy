957dff5df053adb04b183e17b8d5046abaf9da5e
dontmatterbro <yh846175@gmail.com>
2024-09-13 14:49:54 +0300

    added alwayswalk

--- Lua/Scripts/Server/surgery.lua
+++ Lua/Scripts/Server/surgery.lua
@@ -7,7 +7,7 @@ GoodLobotomyAfflictions = {
 BadLobotomyAfflictions = {
 	"lobo_infinitepsychosis", "lobo_mute", "lobo_blurredvision", "lobo_ungenius", "lobo_alwaysdrunk", "lobo_hearscreams", "lobo_tinnitus",
 	"lobo_screenshake", "lobo_deaf", "lobo_blind", "lobo_constantpain", "lobo_paralysis", "lobo_invertcontrols", "lobo_nausea", "lobo_alwaysvigorous",
-	"lobo_alwaysjolly", "lobo_differentteam", "lobo_veryslow", "lobo_alwaysrun", "lobo_randomarrest", "lobo_makescreams", "lobo_fart", "lobo_randomuncon",
+	"lobo_alwaysjolly", "lobo_differentteam", "lobo_veryslow", "lobo_alwaysrun", "lobo_alwayswalk", "lobo_randomarrest", "lobo_makescreams", "lobo_fart", "lobo_randomuncon",
 	"lobo_noanalgesia", "lobo_durden"
 	
 }
